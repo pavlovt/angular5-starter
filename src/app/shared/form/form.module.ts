ecd4a8a424c3b7e21330d27b927b0d829bc452c4
SOFTSERVE\sfili <sfili@softserveinc.com>
2018-04-24 10:13:18 +0300

    add common module

--- src/app/shared/form/form.module.ts
+++ src/app/shared/form/form.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import _ from 'lodash';
 import { AutoCompleteModule, ButtonModule, CalendarModule, CaptchaModule, CheckboxModule, ColorPickerModule, 
   DropdownModule, FieldsetModule, FileUploadModule, InplaceModule, InputMaskModule, InputTextModule, InputSwitchModule, 
@@ -14,7 +16,8 @@ import { XInput } from './components/xinput/xinput'; export { XInput };
     AutoCompleteModule, ButtonModule, CalendarModule, CaptchaModule, CheckboxModule, ColorPickerModule, 
     DropdownModule, FieldsetModule, FileUploadModule, InplaceModule, InputMaskModule, InputTextModule, InputSwitchModule, 
     InputTextareaModule, MultiSelectModule, PasswordModule, ProgressBarModule, ProgressSpinnerModule, RadioButtonModule, RatingModule, 
-    SplitButtonModule, SelectButtonModule, TriStateCheckboxModule, ToggleButtonModule
+    SplitButtonModule, SelectButtonModule, TriStateCheckboxModule, ToggleButtonModule,
+    ReactiveFormsModule, FormsModule, CommonModule,
   ],
   providers: [],
   exports: [
