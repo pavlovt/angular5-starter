3ec2003b5e0601106316c6f1740a109bbc842caa
SOFTSERVE\sfili <sfili@softserveinc.com>
2018-04-24 09:58:26 +0300

    add validation manager

--- src/app/shared/form/components/xinput/xinput.ts
+++ src/app/shared/form/components/xinput/xinput.ts
@@ -8,10 +8,21 @@ declare var $;
     templateUrl: './xinput.html'
 })
 export class XInput implements OnInit {
-
+    
+    @Input() name: string;
+    @Input() label: string = '';
+    @Input() tooltip?: string = '';
+    @Input() placeholder: string = '';
+    @Input() type: string = 'text';
+    @Input() id: string;
+    @Input() form: any = {};
+    @Input() labelClass = '';
+    @Input() maxLenght: number = 9999;
+    @Input() isDisabled: boolean = false;
+    
     constructor() {}
 
     ngOnInit() {
-     
+             
     }
 }
